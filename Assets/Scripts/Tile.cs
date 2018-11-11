ce2fd05bb14f12b5e69e141a2bb2668fb8b51b02
Nicusor97 <nicolaepicatureanu@gmail.com>
2018-11-11 12:17:44 +0200

    Added tile and tile manager scripts to recognize all types of enemies

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+
+public class Tile: MonoBehaviour {
+
+    [SerializeField] private short row;
+    [SerializeField] private short column;
+
+    private Color markColor = new Color(0.929f, 0.850f, 0.670f);
+    private Color defaultColor = new Color(1.0f, 1.0f, 1.0f);
+
+    private Color blueColor = new Color(0, 0, 1, 0.6f);
+    private Color redColor = new Color(0.615f, 0.027f, 0.054f, 0.7f);
+
+    private SpriteRenderer spriteRenderer;
+    private bool isReadyToStep;
+    private PlayerSoldier soldier;
+
+    private PlayerSoldier attackingZombie;
+
+    public short Row { get { return row; } }
+    public short Column { get { return column; } }
+    public PlayerSoldier Soldier { get { return soldier; } set { soldier = value; } }
+
+    private void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void ResetTile() {
+        if(Column < 4) {
+            tag = "BuildTile";
+        }
+        else if(Column > 7) {
+            tag = "EnemyTile";
+        }
+        else {
+            tag = "Tile";
+        }
+        isReadyToStep = false;
+        Soldier = attackingZombie = null;
+        UnColorTile();
+    }
+
+
+    public void MarkTileInUse() {
+        tag = "BuildTileFull";
+    }
+
+    public void UnmarkTileInUse() {
+        tag = "BuildTile";
+    }
+
+    public bool IsMarked() {
+        return spriteRenderer.color == markColor;
+    }
+
+    public void ColorTile() {
+        spriteRenderer.color = markColor;
+    }
+
+    public void UnColorTile() {
+        spriteRenderer.color = defaultColor;
+    }
+
+    public void MarkSoldierSide() {
+        if(spriteRenderer.color != markColor)
+            spriteRenderer.color = soldier.CurrentSide == GameSide.LeftSide ? blueColor : redColor;
+    }
+
+    public void ReadyToStep(PlayerSoldier zombie, bool isPc = false) {
+        isReadyToStep = true;
+        if(!isPc) ColorTile();
+        //if(soldier == null || !soldier.IsEnemy(zombie)) {
+        //    soldier = zombie;
+        //}
+        //else {
+        //    attackingZombie = zombie as Zombie;
+        //}
+        if(soldier != null && soldier.IsEnemy(zombie)) {
+            attackingZombie = zombie as Zombie;
+        }
+        else if(soldier == null) {
+            soldier = zombie;
+        }
+    }
+
+    public void UnReadyToStep(PlayerSoldier zombie, bool isPc = false) {
+        isReadyToStep = false;
+        if(!isPc) UnColorTile();
+        if(soldier != null && zombie == soldier)
+            soldier = null;
+        attackingZombie = null;
+    }
+
+    public void OnMouseDown() {
+        //if(soldier != null) {
+        //    Debug.LogError(soldier.name + " " + soldier.CurrentSide.ToString() + " Rank = " + soldier.Rank + "isReadyToStep = " + isReadyToStep);
+        //}
+        //else {
+        //    Debug.LogError("soldier is NULL, isReadyToStep = " + isReadyToStep);
+        //}
+        if(isReadyToStep && !Globals.IS_SINGLE_PLAYER) {
+            GameManager.Instance.PassTurn(attackingZombie == null ? soldier.CurrentTile : attackingZombie.CurrentTile, this);
+        }
+        MakeStep();
+    }
+
+    public void MakeStep() {
+        if(isReadyToStep) {
+            isReadyToStep = false;
+
+            if(attackingZombie != null && attackingZombie is Zombie) {
+                (attackingZombie as Zombie).GetCloser(soldier);
+                attackingZombie = null;
+            }
+
+            else if(soldier is Zombie) {
+                (soldier as Zombie).Walk(this);
+            }
+            if(Globals.IS_SINGLE_PLAYER && !(soldier is Flag) ) {
+                GameManager.Instance.PassTurn();
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager: Singleton<TileManager> {
+
+    private Dictionary<int, Tile> buildTiles;    // all build tiles
+    private Dictionary<int, Tile> allTiles;      // all tiles
+    private List<Tile> potentialFlagTiles;
+    private Tile[,] matrixTiles;
+
+    public Dictionary<int, Tile> BuildTiles { get { return buildTiles; } }
+    public Dictionary<int, Tile> AllTiles { get { return allTiles; } }
+    public List<Tile> PotentialFlagTiles {  get { return potentialFlagTiles; } }
+    public Tile[,] MatrixTiles { get { return matrixTiles; } }
+
+
+    private void Awake() {
+        DontDestroyOnLoad(this);
+        Init();
+    }
+
+    private void Init() {
+        buildTiles = new Dictionary<int, Tile>();
+        allTiles = new Dictionary<int, Tile>();
+        matrixTiles = new Tile[Globals.ROWS, Globals.COLUMNS];
+
+        var buildTilesGameObjects = GameObject.FindGameObjectsWithTag("BuildTile");
+        foreach(var tile in buildTilesGameObjects) {
+            buildTiles.Add(tile.GetHashCode(), tile.GetComponent<Tile>());
+        }
+
+        var tilesGameObjects = FindObjectsOfType<Tile>();
+        foreach(var tile in tilesGameObjects) {
+            allTiles.Add(tile.GetHashCode(), tile);
+            matrixTiles[tile.Row, tile.Column] = tile;
+        }
+
+        potentialFlagTiles = GetAllPotentialFlagTiles();
+    }
+
+    public void ClearTiles() {
+        foreach(var tile in allTiles.Values) {
+            tile.ResetTile();
+        }
+    }
+
+    public void MarkAvailableBuildTiles() {
+        foreach(var tile in buildTiles.Values) {
+            if(tile.tag == "BuildTile") {
+                tile.ColorTile();
+            }
+        }
+    }
+
+    public void UnmarkAvailableBuildTiles() {
+        foreach(var tile in buildTiles.Values) {
+            tile.UnColorTile();
+        }
+    }
+
+    public void RenameTagsBuildTiles() {
+        foreach(var tile in buildTiles.Values) {
+            tile.UnmarkTileInUse();
+        }
+    }
+
+    public Tile GetNextAvailableTile() {
+        foreach(var tile in buildTiles.Values) {
+            if(tile.tag == "BuildTile") return tile;
+        }
+        return null;
+    }
+
+    public List<Tile> GetAllEnemyTiles() {
+        List<Tile> tiles = new List<Tile>();
+
+        foreach(var tile in allTiles.Values) {
+            if(tile.tag == "EnemyTile")
+                tiles.Add(tile);
+        }
+
+        return tiles;
+    }
+
+    private List<Tile> GetAllPotentialFlagTiles() {
+        List<Tile> tiles = new List<Tile>();
+        for(int i = 0; i < Globals.ROWS; i++) {
+            if(matrixTiles[i, Globals.COLUMNS - 1].tag == "EnemyTile")
+                tiles.Add(matrixTiles[i, Globals.COLUMNS - 1]);
+        }
+        return tiles;
+    }
+
+    public List<Tile> GetClosestTiles(Tile tile, Zombie zombie) {
+        List<Tile> tiles = new List<Tile>();
+        Tile tempTile = null;
+
+        // Check up tile
+        if(tile.Row == 0) {
+            tempTile = null;
+        }
+        else tempTile = matrixTiles[tile.Row - 1, tile.Column];
+        if(tempTile != null && (tempTile.Soldier == null || tempTile.Soldier.IsEnemy(zombie))) {
+            tiles.Add(tempTile);
+        }
+
+        // Check right tile
+        if(tile.Column + 1 == Globals.COLUMNS) {        
+            tempTile = null;
+        }
+        else tempTile = matrixTiles[tile.Row, tile.Column + 1];
+        if(tempTile != null && (tempTile.Soldier == null || tempTile.Soldier.IsEnemy(zombie) )) {
+            tiles.Add(tempTile);
+        }
+
+        // Check down tile
+        if(tile.Row + 1 == Globals.ROWS) {
+            tempTile = null;
+        }
+        else tempTile = matrixTiles[tile.Row + 1, tile.Column];
+        if(tempTile != null && (tempTile.Soldier == null || tempTile.Soldier.IsEnemy(zombie))) {
+            tiles.Add(tempTile);
+        }
+
+        // Check left tile
+        if(tile.Column == 0) {
+            tempTile = null;
+        }
+        else tempTile = matrixTiles[tile.Row, tile.Column - 1];
+        if(tempTile != null && (tempTile.Soldier == null || tempTile.Soldier.IsEnemy(zombie))) {
+            tiles.Add(tempTile);
+        }
+
+        return tiles;
+    }
+
+    public void Fix() {
+        foreach(var tile in allTiles.Values) {
+            if(tile.Soldier != null && tile.Soldier.CurrentTile != tile) {
+                print("fixedIntegrity");
+                tile.Soldier = null;
+            }
+        }
+    }
+}
