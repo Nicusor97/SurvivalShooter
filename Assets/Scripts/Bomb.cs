1e657141536957042322156ff1491147b74f52b7
Nicusor97 <nicolaepicatureanu@gmail.com>
2018-11-25 17:42:35 +0200

    added the bomb and flag logical implemetantation

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Bomb : PlayerSoldier {
+
+    public IEnumerator Explode(bool isExplode) {
+        if(isExplode) {
+            anim.Play("Explode");
+            SoundManager.Instance.SFX.PlayOneShot(SoundManager.Instance.BombExplode);
+            yield return new WaitForSeconds(1.5f);
+        }
+        else {
+            SoundManager.Instance.SFX.PlayOneShot(SoundManager.Instance.BombBought);
+        }
+        SoldierManager.Instance.RemoveSoldierFromList(this);
+
+        GameManager.Instance.UpdateStats();
+        GameManager.Instance.CloseInfo();
+        Destroy(gameObject);
+        CurrentTile = null;
+    }
+
+    public override void SoldierPlacedInEditMode(bool isSoundActivated) {
+        if(++StrategyEditor.NumOfBombs == Globals.MAX_BOMBS) {
+            GameView.DisableButton("Btn_Bomb");
+        }
+        if(isSoundActivated)
+            MakeNoise();
+    }
+
+    public override void MakeNoise() {
+        SoundManager.Instance.SFX.PlayOneShot(SoundManager.Instance.BombBought);
+    }
+
+    public override void SetWarTag() {
+        gameObject.tag = "BombInWar";
+    }
+}
---
+++ Assets/Scripts/Flag.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+
+public class Flag: PlayerSoldier {
+
+    [SerializeField] private Animator blueFlagAnimator;
+    [SerializeField] private Animator redFlagAnimator;
+
+    public override void SoldierPlacedInEditMode(bool isSoundActivated) {
+        StrategyEditor.HasFlag = true;
+        GameView.DisableButton("Btn_Flag");
+        if(isSoundActivated)
+            MakeNoise();
+    }
+
+    public override void MakeNoise() {
+        SoundManager.Instance.SFX.PlayOneShot(SoundManager.Instance.FlagBought);
+    }
+
+    public override void FlipSide(bool isChangeSide = true) {
+        base.FlipSide();
+        OriginAnim = Anim.runtimeAnimatorController = CurrentSide == GameSide.LeftSide ?
+            blueFlagAnimator.runtimeAnimatorController :
+            redFlagAnimator.runtimeAnimatorController;
+    }
+
+    public override void SetWarTag() {
+        gameObject.tag = "FlagInWar";
+    }
+}
