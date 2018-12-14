caee7297953b2b45eac4ec0242409e1b25ba729d
Nicusor97 <nicolaepicatureanu@gmail.com>
2018-12-14 16:15:42 +0200

    added some script used for LAN functionality

---
+++ Assets/Scripts/PlayerSoldier.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+
+public abstract class PlayerSoldier: MonoBehaviour {
+
+    [SerializeField] protected short rank;
+    [SerializeField] private int price;
+    [SerializeField] protected float offset_x;
+    [SerializeField] protected float offset_y;
+    [SerializeField] protected string soldierName;
+    [SerializeField] protected string description;
+    [SerializeField] protected string specialAbilityDescription;
+
+    private StrategyEditor strategyEditor;
+    protected SpriteRenderer spriteRenderer;
+    private Color blueColor = new Color(0, 0, 1, 0.6f);
+    private Color redColor = new Color(1, 0, 0, 0.6f);
+    private Color defaultColor = new Color(1, 1, 1, 1);
+    protected float originOffsetX, originOffsetY;
+
+
+    protected Vector3 originPosition;
+    protected Animator anim;
+    protected RuntimeAnimatorController originAnim;
+    protected PolygonCollider2D playerCollider;
+    protected bool isHidden;
+    protected Sprite sprite;
+
+    private float clickTime;
+
+    public short Rank { get { return rank; } set { rank = value; } }
+    public int Price { get { return price; } }
+    public float OffsetX { get { return offset_x; } }
+    public float OffsetY { get { return offset_y; } }
+    public Tile CurrentTile { get; set; }
+    public GameSide CurrentSide { get; set; }
+    public bool IsHidden { get { return isHidden; } }
+    public Animator Anim { get { return anim; } set { anim = value; } }
+    public RuntimeAnimatorController OriginAnim { get { return originAnim; } set { originAnim = value; } }
+
+    public PolygonCollider2D PlayerCollider { get { return playerCollider; } }
+    public Vector3 OriginPosition { get { return originPosition; } }
+    public Sprite Sprite { get { return sprite; } }
+    public string SoldierName { get { return soldierName; } }
+    public string Description { get { return description; } }
+    public string SpecialAbilityDescription { get { return specialAbilityDescription; } }
+
+    public abstract void SoldierPlacedInEditMode(bool isSoundActivated);
+    public abstract void MakeNoise();
+    public abstract void SetWarTag();
+
+    private void Awake() {
+        anim = GetComponent<Animator>();
+        originAnim = anim.runtimeAnimatorController;
+        playerCollider = GetComponent<PolygonCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        sprite = spriteRenderer.sprite;
+        strategyEditor = StrategyEditor.Instance;
+        originOffsetX = offset_x;
+        originOffsetY = offset_y;
+    }
+
+    public virtual void FlipSide(bool isChangeSide = true) {
+        offset_x = originOffsetX = -offset_x;
+        transform.localScale = new Vector3(-transform.localScale.x, 1, 1);
+        if(isChangeSide) CurrentSide = CurrentSide == GameSide.LeftSide ? GameSide.RightSide : GameSide.LeftSide;
+    }
+
+    public bool IsEnemy(PlayerSoldier enemy) {
+        return CurrentSide != enemy.CurrentSide;
+    }
+
+    protected void OnMouseDown() {
+        clickTime = Time.time;
+        if(strategyEditor != null && strategyEditor.PlayerBtnPressed == null && StrategyEditor.IsInEdit) {
+            originPosition = transform.position;
+            TileManager.Instance.MarkAvailableBuildTiles();
+        }
+    }
+
+    protected void OnMouseDrag() {      //Use for dragging in edit mode, and also for long click during the game
+        if(strategyEditor != null && strategyEditor.PlayerBtnPressed == null && StrategyEditor.IsInEdit) {
+            var mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+            transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
+        }
+        else if(!isHidden && Globals.IS_IN_GAME && Mathf.Abs(Time.time - clickTime) > .3f && !GameManager.Instance.IsDescriptionOpen) {
+            clickTime = Time.time;
+            StartCoroutine(GameManager.Instance.DisplayInfo(this));
+        }
+    }
+
+    protected void OnMouseUp() {
+        if(strategyEditor != null && strategyEditor.PlayerBtnPressed == null && StrategyEditor.IsInEdit) {
+            TileManager.Instance.UnmarkAvailableBuildTiles();
+            if(strategyEditor.ChangeSoldierPosition(this)) {
+                MakeNoise();
+                originPosition = transform.position;
+            }
+            else {
+                transform.position = originPosition;
+            }
+        }
+    }
+
+    public void HideSoldier(Animator templateAnimator, float offsetX, float offsetY) {
+        if(!isHidden) {
+            isHidden = true;
+            Anim.runtimeAnimatorController = templateAnimator.runtimeAnimatorController;
+            spriteRenderer.color = CurrentSide == GameSide.LeftSide ? blueColor : redColor;
+            offset_x = CurrentSide == GameSide.LeftSide ? offsetX : -offsetX;
+            offset_y = offsetY;
+            transform.position = new Vector2(CurrentTile.transform.position.x + offset_x, CurrentTile.transform.position.y + offset_y);
+        }
+    }
+
+    public void CoverSoldier() {
+        if(isHidden) {
+            isHidden = false;
+            Anim.runtimeAnimatorController = OriginAnim;
+            spriteRenderer.color = defaultColor;
+            offset_x = originOffsetX;
+            offset_y = originOffsetY;
+            transform.position = new Vector2(CurrentTile.transform.position.x + offset_x, CurrentTile.transform.position.y + offset_y);
+        }
+    }
+}
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+//T must inherits from MonoBehaviour
+public class Singleton<T>: MonoBehaviour where T : MonoBehaviour {
+
+    private static T instance;
+
+    public static T Instance {
+        get {
+            T memorizedObject = FindObjectOfType<T>();
+            if(instance == null) {
+                instance = memorizedObject;
+                //DontDestroyOnLoad(instance);
+            }
+            else if(instance != memorizedObject)
+                Destroy(memorizedObject);
+            return instance;
+        }
+    }
+
+}
